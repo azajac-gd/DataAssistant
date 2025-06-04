af4e5cd8ec5aa7e847947b2206a82e02631d50bb
azajac-gd <azajac@griddynamics.com>
2025-06-04 11:11:41 +0200

    Add cleanup function to remove plot files on exit

--- src/app/main.py
+++ src/app/main.py
@@ -7,6 +7,7 @@ from data_generation import show_data_generation
 from talk_to_data import show_talk_to_data
 from langfuse.decorators import observe
 from dotenv import load_dotenv
+import atexit
 load_dotenv()
 
 logging.basicConfig(
@@ -24,6 +25,15 @@ def main():
     elif selection == "Talk to Your Data":
         show_talk_to_data()
 
+def cleanup_plots():
+    folder = "plots"
+    if os.path.exists(folder):
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+
 if __name__ == "__main__":
+    atexit.register(cleanup_plots)
     main()
 
